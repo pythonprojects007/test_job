1a2b6c9833fe2ef7cd66ad7c798a2cd2b0ac8811
Vipin <pythonprojects007@gmail.com>
2023-05-31 22:02:18 +0530

    its for jenkins job

---
+++ main_module.py
@@ -0,0 +1,5 @@
+import sub_module
+
+obj = sub_module.AddNumbers(7, 8)
+result = obj.add_two_numbers()
+print(result)
---
+++ sub_module.py
@@ -0,0 +1,9 @@
+
+class AddNumbers:
+    def __init__(self, number1, number2):
+        self.number1 = number1
+        self.number2 = number2
+
+    def add_two_numbers(self):
+        result = self.number1 + self.number2
+        return result
